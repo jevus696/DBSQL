b9ce20a07044345cb2118f5a89a67f8765ffc710
jevus696 <jesus_garciasaucedo@uadec.edu.mx>
2017-02-28 00:56:58 -0600

    Pruebas de Funcionamiento

--- DBSQL/Form1.Designer.cs
+++ DBSQL/Form1.Designer.cs
@@ -28,12 +28,340 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Cnx = new System.Data.OleDb.OleDbConnection();
+            this.BtnCargarFoto = new System.Windows.Forms.Button();
+            this.TxtId = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtFechaAlta = new System.Windows.Forms.TextBox();
+            this.TxtNIF = new System.Windows.Forms.TextBox();
+            this.TxtFechaNac = new System.Windows.Forms.TextBox();
+            this.TxtDireccion = new System.Windows.Forms.TextBox();
+            this.TxtPoblacion = new System.Windows.Forms.TextBox();
+            this.TxtTelefono = new System.Windows.Forms.TextBox();
+            this.TxtEstadoCivil = new System.Windows.Forms.TextBox();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.BtnModificar = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Location = new System.Drawing.Point(482, 355);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(75, 23);
+            this.BtnSalir.TabIndex = 0;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(380, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Cnx
+            // 
+            this.Cnx.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"C:\\Users\\Jesus\\Documents\\Visual Stu" +
+    "dio 2015\\Projects\\DBSQL\\Vendedores.mdb\"";
+            // 
+            // BtnCargarFoto
+            // 
+            this.BtnCargarFoto.Location = new System.Drawing.Point(380, 207);
+            this.BtnCargarFoto.Name = "BtnCargarFoto";
+            this.BtnCargarFoto.Size = new System.Drawing.Size(140, 23);
+            this.BtnCargarFoto.TabIndex = 2;
+            this.BtnCargarFoto.Text = "Cargar Foto";
+            this.BtnCargarFoto.UseVisualStyleBackColor = true;
+            this.BtnCargarFoto.Click += new System.EventHandler(this.BtnCargarFoto_Click);
+            // 
+            // TxtId
+            // 
+            this.TxtId.Location = new System.Drawing.Point(122, 38);
+            this.TxtId.Name = "TxtId";
+            this.TxtId.Size = new System.Drawing.Size(192, 20);
+            this.TxtId.TabIndex = 3;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(122, 64);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(192, 20);
+            this.TxtNombre.TabIndex = 4;
+            // 
+            // TxtFechaAlta
+            // 
+            this.TxtFechaAlta.Location = new System.Drawing.Point(122, 90);
+            this.TxtFechaAlta.Name = "TxtFechaAlta";
+            this.TxtFechaAlta.Size = new System.Drawing.Size(192, 20);
+            this.TxtFechaAlta.TabIndex = 5;
+            // 
+            // TxtNIF
+            // 
+            this.TxtNIF.Location = new System.Drawing.Point(122, 116);
+            this.TxtNIF.Name = "TxtNIF";
+            this.TxtNIF.Size = new System.Drawing.Size(192, 20);
+            this.TxtNIF.TabIndex = 6;
+            // 
+            // TxtFechaNac
+            // 
+            this.TxtFechaNac.Location = new System.Drawing.Point(122, 142);
+            this.TxtFechaNac.Name = "TxtFechaNac";
+            this.TxtFechaNac.Size = new System.Drawing.Size(192, 20);
+            this.TxtFechaNac.TabIndex = 7;
+            // 
+            // TxtDireccion
+            // 
+            this.TxtDireccion.Location = new System.Drawing.Point(122, 168);
+            this.TxtDireccion.Name = "TxtDireccion";
+            this.TxtDireccion.Size = new System.Drawing.Size(192, 20);
+            this.TxtDireccion.TabIndex = 8;
+            // 
+            // TxtPoblacion
+            // 
+            this.TxtPoblacion.Location = new System.Drawing.Point(122, 194);
+            this.TxtPoblacion.Name = "TxtPoblacion";
+            this.TxtPoblacion.Size = new System.Drawing.Size(192, 20);
+            this.TxtPoblacion.TabIndex = 9;
+            // 
+            // TxtTelefono
+            // 
+            this.TxtTelefono.Location = new System.Drawing.Point(122, 220);
+            this.TxtTelefono.Name = "TxtTelefono";
+            this.TxtTelefono.Size = new System.Drawing.Size(192, 20);
+            this.TxtTelefono.TabIndex = 10;
+            // 
+            // TxtEstadoCivil
+            // 
+            this.TxtEstadoCivil.Location = new System.Drawing.Point(122, 246);
+            this.TxtEstadoCivil.Name = "TxtEstadoCivil";
+            this.TxtEstadoCivil.Size = new System.Drawing.Size(192, 20);
+            this.TxtEstadoCivil.TabIndex = 11;
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(401, 355);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.BtnLimpiar.TabIndex = 12;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click_1);
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Location = new System.Drawing.Point(320, 355);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.BtnAgregar.TabIndex = 13;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(239, 355);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.BtnBuscar.TabIndex = 14;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click_1);
+            // 
+            // BtnModificar
+            // 
+            this.BtnModificar.Location = new System.Drawing.Point(158, 355);
+            this.BtnModificar.Name = "BtnModificar";
+            this.BtnModificar.Size = new System.Drawing.Size(75, 23);
+            this.BtnModificar.TabIndex = 15;
+            this.BtnModificar.Text = "Modificar";
+            this.BtnModificar.UseVisualStyleBackColor = true;
+            this.BtnModificar.Click += new System.EventHandler(this.BtnModificar_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(77, 355);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.BtnEliminar.TabIndex = 16;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Id Vendedor :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Fecha Alta :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "NIF :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Fecha Nacimiento :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Dirección :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Población :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Telefono :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 249);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Estado Civil :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(569, 390);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnModificar);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.TxtEstadoCivil);
+            this.Controls.Add(this.TxtTelefono);
+            this.Controls.Add(this.TxtPoblacion);
+            this.Controls.Add(this.TxtDireccion);
+            this.Controls.Add(this.TxtFechaNac);
+            this.Controls.Add(this.TxtNIF);
+            this.Controls.Add(this.TxtFechaAlta);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.TxtId);
+            this.Controls.Add(this.BtnCargarFoto);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnSalir);
+            this.Name = "Form1";
+            this.Text = "Conexión a DB Access";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Data.OleDb.OleDbConnection Cnx;
+        private System.Windows.Forms.Button BtnCargarFoto;
+        private System.Windows.Forms.TextBox TxtId;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtFechaAlta;
+        private System.Windows.Forms.TextBox TxtNIF;
+        private System.Windows.Forms.TextBox TxtFechaNac;
+        private System.Windows.Forms.TextBox TxtDireccion;
+        private System.Windows.Forms.TextBox TxtPoblacion;
+        private System.Windows.Forms.TextBox TxtTelefono;
+        private System.Windows.Forms.TextBox TxtEstadoCivil;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Button BtnModificar;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- DBSQL/Form1.cs
+++ DBSQL/Form1.cs
@@ -1,19 +1,201 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
-namespace DBSQL
+namespace DBSQL 
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string StrFileName;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLimpiar_Click_1(object sender, EventArgs e)
+        {
+            TxtId.Clear();
+            TxtNombre.Clear();
+            TxtFechaAlta.Clear();
+            TxtNIF.Clear();
+            TxtFechaNac.Clear();
+            TxtDireccion.Clear();
+            TxtPoblacion.Clear();
+            TxtTelefono.Clear();
+            TxtEstadoCivil.Clear();
+            pictureBox1.ImageLocation = "";
+            TxtId.Focus();
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                const string insertar = @"Insert Into Vendedores Values (@IdVendedor, @NombreVendedor,
+                @FechaAlta, @NIF, @FechaNac,@Direccion,@Poblacion,@Telefono,@EstadoCivil,@Fotografia)";
+                var cmd = new OleDbCommand(insertar, Cnx) { CommandType = CommandType.Text };
+                cmd.Parameters.AddWithValue("@IdVendedor", TxtId.Text);
+                cmd.Parameters.AddWithValue("@NombreVendedor", TxtNombre.Text);
+                cmd.Parameters.AddWithValue("@FechaAlta", TxtFechaAlta.Text);
+                cmd.Parameters.AddWithValue("@NIF", TxtNIF.Text);
+                cmd.Parameters.AddWithValue("@FechaNac", TxtFechaNac.Text);
+                cmd.Parameters.AddWithValue("@Direccion", TxtDireccion.Text);
+                cmd.Parameters.AddWithValue("@Poblacion", TxtPoblacion.Text);
+                cmd.Parameters.AddWithValue("@Telefono", TxtTelefono.Text);
+                cmd.Parameters.AddWithValue("@EstadoCivil", TxtEstadoCivil.Text);
+                pictureBox1.ImageLocation = StrFileName;
+                cmd.Parameters.AddWithValue("@Fotografia", pictureBox1.ImageLocation);
+                Cnx.Open();
+                cmd.ExecuteNonQuery();
+                Cnx.Close();
+                MessageBox.Show(@"El Contacto Fue Registrado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(@"La Clave a Registrar Ya Existe", ex.Message);
+            }
+            TxtId.Clear();
+            TxtNombre.Clear();
+            TxtFechaAlta.Clear();
+            TxtNIF.Clear();
+            TxtFechaNac.Clear();
+            TxtDireccion.Clear();
+            TxtPoblacion.Clear();
+            TxtTelefono.Clear();
+            TxtEstadoCivil.Clear();
+            pictureBox1.ImageLocation = "";
+            TxtId.Focus();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnBuscar_Click_1(object sender, EventArgs e)
+        {
+            const string buscar = "SELECT * FROM Vendedores WHERE IdVendedor=@Id";
+            var cmdBuscar = new OleDbCommand(buscar, Cnx) { CommandType = CommandType.Text };
+            cmdBuscar.Parameters.AddWithValue("@Id", TxtId.Text);
+            Cnx.Open();
+            var lectura = cmdBuscar.ExecuteReader();
+            if (lectura != null && lectura.Read() == true)
+            {
+                TxtNombre.Text = lectura[1].ToString();
+                TxtFechaAlta.Text = lectura[2].ToString();
+                TxtNIF.Text = lectura[3].ToString();
+                TxtFechaNac.Text = lectura[4].ToString();
+                TxtDireccion.Text = lectura[5].ToString();
+                TxtPoblacion.Text = lectura[6].ToString();
+                TxtTelefono.Text = lectura[7].ToString();
+                TxtEstadoCivil.Text = lectura[8].ToString();
+                pictureBox1.ImageLocation = lectura[9].ToString();
+            }
+            else
+            {
+                MessageBox.Show(@"Los Datos a Buscar No Estan Registrados");
+                TxtId.Clear();
+                TxtId.Focus();
+            }
+            Cnx.Close();
+        }
+
+        private void BtnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                const string actualizar = @"UPDATE Vendedores SET IdVendedor=@Id, NombreVendedor=@Nombre ,FechaAlta = @FechaAlta,
+                                    NIF = @NIF, FechaNac = @FechaNac, Direccion = @Direccion, Poblacion = @Poblacion,
+                                    Telefon = @Telefon, EstalCivil = @EstalCivil,Fotografia = @Fotografia WHERE IdVendedor = @Id";
+                var cmd = new OleDbCommand(actualizar, Cnx) { CommandType = CommandType.Text };
+                cmd.Parameters.AddWithValue("@Id", TxtId.Text);
+                cmd.Parameters.AddWithValue("@Nombre", TxtNombre.Text);
+                cmd.Parameters.AddWithValue("@FechaAlta", TxtFechaAlta.Text);
+                cmd.Parameters.AddWithValue("@NIF", TxtNIF.Text);
+                cmd.Parameters.AddWithValue("@FechaNac", TxtFechaNac.Text);
+                cmd.Parameters.AddWithValue("@Direccion", TxtDireccion.Text);
+                cmd.Parameters.AddWithValue("@Poblacion", TxtPoblacion.Text);
+                cmd.Parameters.AddWithValue("@Telefon", TxtTelefono.Text);
+                cmd.Parameters.AddWithValue("@EstalCivil", TxtEstadoCivil.Text);
+                cmd.Parameters.AddWithValue("@Fotografia", pictureBox1.ImageLocation);
+                Cnx.Open();
+                cmd.ExecuteNonQuery();
+                Cnx.Close();
+                MessageBox.Show(@"Los Datos Del Contacto Fueron Actualizados");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            TxtId.Clear();
+            TxtNombre.Clear();
+            TxtFechaAlta.Clear();
+            TxtNIF.Clear();
+            TxtFechaNac.Clear();
+            TxtDireccion.Clear();
+            TxtPoblacion.Clear();
+            TxtTelefono.Clear();
+            TxtEstadoCivil.Clear();
+            pictureBox1.ImageLocation = "";
+            TxtId.Focus();
+
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                const string eliminar = "DELETE FROM Vendedores WHERE IdVendedor=@Id";
+                var cmd = new OleDbCommand(eliminar, Cnx) { CommandType = CommandType.Text };
+                cmd.Parameters.AddWithValue("@Id", TxtId.Text);
+                Cnx.Open();
+                cmd.ExecuteNonQuery();
+                Cnx.Close();
+                MessageBox.Show(@"El Contacto Fue Eliminado....");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            TxtId.Clear();
+            TxtNombre.Clear();
+            TxtFechaAlta.Clear();
+            TxtNIF.Clear();
+            TxtFechaNac.Clear();
+            TxtDireccion.Clear();
+            TxtPoblacion.Clear();
+            TxtTelefono.Clear();
+            TxtEstadoCivil.Clear();
+            pictureBox1.ImageLocation = "";
+            TxtId.Focus();
+        }
+
+        private void BtnCargarFoto_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1 == null) return;
+            openFileDialog1.Filter = @"jpegs|*.jpg|gifs|*.gif|Bitmaps|*.bmp";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Title = @"Abrir un archivo de imagen";
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                StrFileName = openFileDialog1.FileName;
+                pictureBox1.Image = Image.FromFile(StrFileName);
+                openFileDialog1.Reset();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
